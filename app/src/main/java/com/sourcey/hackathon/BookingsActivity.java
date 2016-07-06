9ecfe21f2d0b16f8853c80115f9990422172c5ae
Simon Markham <markhams@tcd.ie>
2016-07-06 17:53:08 +0100

    lol

--- app/src/main/java/com/sourcey/hackathon/BookingsActivity.java
+++ app/src/main/java/com/sourcey/hackathon/BookingsActivity.java
@@ -1,6 +1,12 @@
 package com.sourcey.hackathon;
 
 import com.alamkanak.weekview.WeekViewEvent;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -13,10 +19,66 @@ import java.util.List;
  */
 public class BookingsActivity extends BaseActivity {
 
+    private String getBookingsURL =  "http://10.0.2.2:4000/booking";
+
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
         // Populate the week view with some events.
         List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
+//        for(int i = 0; i < 7; i++){
+//            for(int j = 9; j < 21; j++){
+//                Calendar startTime = Calendar.getInstance();
+//                startTime.set(Calendar.DAY_OF_WEEK, i);
+//                startTime.set(Calendar.HOUR_OF_DAY, j);
+//                startTime.set(Calendar.MINUTE, 0);
+//                startTime.set(Calendar.MONTH, newMonth);
+//                startTime.set(Calendar.YEAR, newYear);
+//
+//                Calendar endTime = (Calendar) startTime.clone();
+//                endTime.add(Calendar.HOUR, j + 1);
+//                endTime.add(Calendar.MONTH, newMonth);
+//                endTime.add(Calendar.DAY_OF_WEEK, i);
+//                WeekViewEvent event = new WeekViewEvent(i, getEventTitle(startTime), startTime, endTime);
+//                event.setColor(getResources().getColor(R.color.event_color_01));
+//                events.add(event);
+//
+//            }
+//        }
+
+
+//        try{
+//            JSONArray jsonArray = getEvents();
+//
+//            for(int i = 0; i < jsonArray.length();i++){
+//                Integer startMonth = getStartMonth(jsonArray.getJSONObject(i));
+//                Integer endMonth = getEndMonth(jsonArray.getJSONObject(i));
+//                Integer startDay = getStartDay(jsonArray.getJSONObject(i));
+//                Integer endDay = getEndDay(jsonArray.getJSONObject(i));
+//                Integer startYear = getStartYear(jsonArray.getJSONObject(i));
+//                Integer endYear = getEndYear(jsonArray.getJSONObject(i));
+//                Integer startHour = getStartHour(jsonArray.getJSONObject(i));
+//                Integer endHour = getEndHour(jsonArray.getJSONObject(i));
+//
+//                Calendar startTime = Calendar.getInstance();
+//                startTime.set(Calendar.DAY_OF_MONTH, startDay);
+//                startTime.set(Calendar.HOUR_OF_DAY, startHour);
+//                startTime.set(Calendar.MINUTE, 0);
+//                startTime.set(Calendar.MONTH, startMonth);
+//                startTime.set(Calendar.YEAR, startYear);
+//
+//                Calendar endTime = (Calendar) startTime.clone();
+//                endTime.add(Calendar.HOUR, endHour);
+//                endTime.add(Calendar.MONTH, endMonth);
+//                endTime.add(Calendar.DAY_OF_MONTH, endDay);
+//                WeekViewEvent event = new WeekViewEvent(i, getEventTitle(startTime), startTime, endTime);
+//                event.setColor(getResources().getColor(R.color.event_color_01));
+//                events.add(event);
+//            }
+//        } catch(Exception ex) {
+//
+//        }
+
 
         Calendar startTime = Calendar.getInstance();
         startTime.set(Calendar.HOUR_OF_DAY, 3);
@@ -46,125 +108,157 @@ public class BookingsActivity extends BaseActivity {
         event.setColor(getResources().getColor(R.color.event_color_02));
         events.add(event);
 
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 4);
-        startTime.set(Calendar.MINUTE, 20);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.set(Calendar.HOUR_OF_DAY, 5);
-        endTime.set(Calendar.MINUTE, 0);
-        event = new WeekViewEvent(10, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_03));
-        events.add(event);
 
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 5);
-        startTime.set(Calendar.MINUTE, 30);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR_OF_DAY, 2);
-        endTime.set(Calendar.MONTH, newMonth-1);
-        event = new WeekViewEvent(2, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_02));
-        events.add(event);
 
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 5);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth - 1);
-        startTime.set(Calendar.YEAR, newYear);
-        startTime.add(Calendar.DATE, 1);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR_OF_DAY, 3);
-        endTime.set(Calendar.MONTH, newMonth - 1);
-        event = new WeekViewEvent(3, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_03));
-        events.add(event);
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.DAY_OF_MONTH, 15);
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR_OF_DAY, 3);
-        event = new WeekViewEvent(4, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_04));
-        events.add(event);
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.DAY_OF_MONTH, 1);
-        startTime.set(Calendar.HOUR_OF_DAY, 3);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR_OF_DAY, 3);
-        event = new WeekViewEvent(5, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_01));
-        events.add(event);
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.DAY_OF_MONTH, startTime.getActualMaximum(Calendar.DAY_OF_MONTH));
-        startTime.set(Calendar.HOUR_OF_DAY, 15);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR_OF_DAY, 3);
-        event = new WeekViewEvent(5, getEventTitle(startTime), startTime, endTime);
-        event.setColor(getResources().getColor(R.color.event_color_02));
-        events.add(event);
-
-        //AllDay event
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.HOUR_OF_DAY, 0);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.add(Calendar.HOUR_OF_DAY, 23);
-        event = new WeekViewEvent(value, getEventTitle(startTime), 2016, 1, 16, 12, 0, 2016, 1, 16, 13, 0);
-        event.setColor(getResources().getColor(R.color.event_color_04));
-        events.add(event);
-        events.add(event);
-        value++;
-
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.DAY_OF_MONTH, 8);
-        startTime.set(Calendar.HOUR_OF_DAY, 2);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.set(Calendar.DAY_OF_MONTH, 10);
-        endTime.set(Calendar.HOUR_OF_DAY, 23);
-        //(long id, String name, int startYear, int startMonth, int startDay, int startHour, int startMinute, int endYear, int endMonth, int endDay, int endHour, int endMinute)
-
-        event = new WeekViewEvent(value, getEventTitle(startTime), 2016, 1, 17, 12, 0, 2016, 1, 17, 13, 0);
-        event.setColor(getResources().getColor(R.color.event_color_03));
-        events.add(event);
-        value++;
-
-        // All day event until 00:00 next day
-        startTime = Calendar.getInstance();
-        startTime.set(Calendar.DAY_OF_MONTH, 10);
-        startTime.set(Calendar.HOUR_OF_DAY, 0);
-        startTime.set(Calendar.MINUTE, 0);
-        startTime.set(Calendar.SECOND, 0);
-        startTime.set(Calendar.MILLISECOND, 0);
-        startTime.set(Calendar.MONTH, newMonth-1);
-        startTime.set(Calendar.YEAR, newYear);
-        endTime = (Calendar) startTime.clone();
-        endTime.set(Calendar.DAY_OF_MONTH, 11);
-        event = new WeekViewEvent(value, getEventTitle(startTime), 2016, 1, 18, 12, 0, 2016, 1, 18, 13, 0);
-        event.setColor(getResources().getColor(R.color.event_color_01));
-        events.add(event);
 
         return events;
+    }
+
+
+    private Integer getStartMonth(JSONObject object){
+        try{
+            String date = object.getString("startMonth");
+            String[] dateInfo = date.split("/");
+            String startMonth = dateInfo[1];
+
+            if(startMonth.startsWith("0")){
+                startMonth = startMonth.replaceAll("^0+", "");
+            }
+            return new Integer(startMonth);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    private Integer getStartDay(JSONObject object){
+        try{
+            String date = object.getString("startDate");
+            String[] dateInfo = date.split("/");
+            String startDay = dateInfo[1];
+
+            if(startDay.startsWith("0")){
+                startDay = startDay.replaceAll("^0+", "");
+            }
+            return new Integer(startDay);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    private Integer getStartYear(JSONObject object){
+        try{
+            String date = object.getString("startYear");
+            String[] dateInfo = date.split("/");
+            String startYear = dateInfo[1];
+
+            if(startYear.startsWith("0")){
+                startYear = startYear.replaceAll("^0+", "");
+            }
+            return new Integer(startYear);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    private Integer getEndMonth(JSONObject object){
+        try{
+            String date = object.getString("endMonth");
+            String[] dateInfo = date.split("/");
+            String endMonth = dateInfo[1];
+
+            if(endMonth.startsWith("0")){
+                endMonth = endMonth.replaceAll("^0+", "");
+            }
+            return new Integer(endMonth);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    private Integer getEndDay(JSONObject object){
+        try{
+            String date = object.getString("endDate");
+            String[] dateInfo = date.split("/");
+            String endDay = dateInfo[1];
+
+            if(endDay.startsWith("0")){
+                endDay = endDay.replaceAll("^0+", "");
+            }
+            return new Integer(endDay);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    private Integer getEndYear(JSONObject object){
+        try{
+            String date = object.getString("endYear");
+            String[] dateInfo = date.split("/");
+            String endYear = dateInfo[1];
+
+            if(endYear.startsWith("0")){
+                endYear = endYear.replaceAll("^0+", "");
+            }
+            return new Integer(endYear);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+    private Integer getStartHour(JSONObject object){
+        try{
+            String date = object.getString("startHour");
+            String[] dateInfo = date.split("/");
+            String endDay = dateInfo[1];
+
+            if(endDay.startsWith("0")){
+                endDay = endDay.replaceAll("^0+", "");
+            }
+            return new Integer(endDay);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    private Integer getEndHour(JSONObject object){
+        try{
+            String date = object.getString("endHour");
+            String[] dateInfo = date.split("/");
+            String endYear = dateInfo[1];
+
+            if(endYear.startsWith("0")){
+                endYear = endYear.replaceAll("^0+", "");
+            }
+            return new Integer(endYear);
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    private JSONArray getEvents(){
+        boolean wasSuccess = true;
+        JSONArray array = new JSONArray();
+        try{
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(getBookingsURL)
+                    .get()
+                    .addHeader("content-type", "application/json")
+                    .addHeader("cache-control", "no-cache")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                wasSuccess = true;
+                array = new JSONArray(response.body().string());
+
+            } else {
+                wasSuccess = false;
+            }
+        } catch(Exception ex){
+
+        }
+        return array;
     }
 
 }
